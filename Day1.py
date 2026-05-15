ed544e83faee1afe5daeaec38ac4d5459aa1d6dc
Ilayda <turkishdelightmu@gmail.com>
2026-05-15 11:54:13 +0400

    feat: initial commit with Day1.py

---
+++ Day1.py
@@ -0,0 +1,49 @@
+# Day 1 - 30DaysOfPython Challenge
+
+# Exercise: 1
+# python3 --version
+
+import sys
+print(sys.version)
+
+print(3 + 4)             # addition(+)
+print(4 - 3)             # subtraction(-)
+print(3 * 4)             # multiplication(*)
+print(3 / 4)             # division(/)
+print(3 ** 4)            # exponential(**)
+print(3 % 4)             # modulus(%)
+print(3 // 4)            # Floor division operator(//)
+print('Aisha')
+print('Taneri')
+print('Turkey')
+print('I am enjoying 30 days of python!')
+
+
+# Exercise: 2
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(["Asabeneh", "Finland", "Python"]))
+print(type('Aisha'))
+print(type('Taneri'))
+print(type('Turkey'))
+print(type('I am enjoying 30 days of python!'))
+
+
+# Exercise: 3
+print(29)      # Int
+print(2.14)      # Float
+print(4 + 3j)     # Complex number
+print('Hello')  # String
+print([11, 12, 13])   # List
+print({'name': 'Aisha'})  # Dictionary
+print({1.8, 4.14, 8.7})    # Set
+print((1.8, 4.14, 8.7))   # Tuple
+print(True)  # Boolean
+print(False)  # Boolean
+
+# Euclidean distance
+# Find an Euclidean distance between (2, 3) and (10, 8)
+
+print(((10 - 2) ** 2 + (8 - 3) ** 2) ** 0.5)
